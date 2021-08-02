4402e71273b86a0679d40992e63465c881320728
JBredlau <joerg>
2021-08-02 16:07:19 +0200

    commit for first tests and solutions to convert numeric to roman

--- src/js/MyClass.js
+++ src/js/MyClass.js
@@ -1,7 +1,86 @@
 class MyClass {
-  static myFunction () {
-    return 1
+  static convertNumberToRoman2 (number) {
+    let roman = ''
+    if (number < 1 || number > 3999) {
+      return 'Bitte Nummer zwischen 1 und 3999 eingeben'
+    }
+
+    for (const key in romanNumList) {
+      const flooredNumber = Math.floor(number / romanNumList[key])
+      console.log('Debug flooredNumber: ' + flooredNumber)
+
+      if (flooredNumber >= 0) {
+        for (let i = 0; i < flooredNumber; i++) {
+          roman += key
+        }
+      }
+      number = number % romanNumList[key]
+      console.log('Debug Number: ' + number)
+    }
+    console.log('Debug roman: ' + roman)
+    return roman
+  }
+
+  static convertNumberToRoman (number) {
+    let roman = ''
+    if (number < 1 || number > 3999) {
+      return 'Bitte Nummer zwischen 1 und 3999 eingeben'
+    }
+    for (const key in romanNumList) {
+      while (number >= romanNumList[key]) {
+        roman += key
+        number -= romanNumList[key]
+      }
+    }
+    return roman
+  }
+
+  static convertNumberToRoman1 (number) {
+    if (number === 0) {
+      return '';
+    }
+    if (number < 1 || number > 3999) {
+      return 'Bitte Nummer zwischen 1 und 3999 eingeben'
+    }
+
+    for (let i = 0; i < romanMatrix.length; i++) {
+      if (number >= romanMatrix[i][0]) {
+        return romanMatrix[i][1] + this.convertNumberToRoman(number - romanMatrix[i][0]);
+      }
+    }
   }
 }
+
+const romanNumList = {
+  M: 1000,
+  CM: 900,
+  D: 500,
+  CD: 400,
+  C: 100,
+  XC: 90,
+  L: 50,
+  XL: 40,
+  X: 10,
+  IX: 9,
+  V: 5,
+  IV: 4,
+  I: 1
+}
+
+const romanMatrix = [
+  [1000, 'M'],
+  [900, 'CM'],
+  [500, 'D'],
+  [400, 'CD'],
+  [100, 'C'],
+  [90, 'XC'],
+  [50, 'L'],
+  [40, 'XL'],
+  [10, 'X'],
+  [9, 'IX'],
+  [5, 'V'],
+  [4, 'IV'],
+  [1, 'I']
+]
 
 module.exports = MyClass
--- test/js/MainTest.js
+++ test/js/MainTest.js
@@ -4,9 +4,31 @@ const expect = chai.expect
 const MyClass = require('../../src/js/MyClass')
 
 describe('MyClass', function () {
-  describe('#myFunction', function () {
-    it('should always return 1', function () {
-      expect(MyClass.myFunction()).to.be.equal(1)
+  describe('#convertNumberToRoman', function () {
+    it('should always convert 1 to I', function () {
+      expect(MyClass.convertNumberToRoman(1)).to.be.equal("I")
+    })
+    it('should always convert 2 to II', function () {
+      expect(MyClass.convertNumberToRoman(2)).to.be.equal("II")
+    })
+    it('should always convert 3 to III', function () {
+      expect(MyClass.convertNumberToRoman(3)).to.be.equal("III")
+    })
+    it('should always convert 4 to IV', function () {
+      expect(MyClass.convertNumberToRoman(4)).to.be.equal("IV")
+    })
+    it('should always convert 42 to XLII', function () {
+      expect(MyClass.convertNumberToRoman(42)).to.be.equal("XLII")
+    })
+
+    it('should always convert 1234 to MCCXXXIV', function () {
+      expect(MyClass.convertNumberToRoman(1234)).to.be.equal("MCCXXXIV")
+    })
+
+    it('should return message if number <1 or >3999', function () {
+      expect(MyClass.convertNumberToRoman(5000)).to.be.equal("Bitte Nummer zwischen 1 und 3999 eingeben")
     })
   })
 })
+
+
